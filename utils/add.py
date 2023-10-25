b88bc11d5e4def575c56fa694e57c9841cfc498b
theshreyasm <theshreyasm@gmail.com>
2023-10-26 01:00:19 +0530

    removed unnecessary functions

--- utils/add.py
+++ utils/add.py
@@ -21,38 +21,6 @@ def checkEntry(sitename, url, email, username):
         return True
     return False
 
-def addEntry(masterpassword, device_secret, sitename, url, email, username):
-
-    if(checkEntry(sitename, url, email, username)):
-        print("Entry with these details already exists.")
-        return
-    
-    option = input("Do you want to use a secure system generated password? y or n? ")
-
-    while(option != 'y' and option != 'n'):
-        print("Answer either y or n.")
-        option = input("Do you want to use a secure system generated password? y or n? ")
-
-    if(option == 'n'):
-        password = getpass.getpass("Enter your password: ")
-    else:
-        password = generatePassword()
-        print("Secure password is generated.")
-
-    masterkey = cipher.generateKey(masterpassword, device_secret)
-    encrypted = cipher.encrypt(masterkey, password)
-
-    db = dbconfig()
-    cursor = db.cursor()
-    query = "INSERT INTO passwordmanager.entries (sitename, url, email, username, password) values (%s, %s, %s, %s, %s)"
-    val = (sitename, url, email, username, encrypted)
-    cursor.execute(query, val)
-    db.commit()
-
-    cursor.close()
-    db.close()
-    
-    print("New entry is created.")
 
 def add_row(window, tv):
     
--- utils/retrieve.py
+++ utils/retrieve.py
@@ -3,51 +3,6 @@ import utils.cipher as cipher
 from prettytable import PrettyTable
 import pyperclip
 
-def retrieve(masterpassword, device_secret, details):
-
-    db = dbconfig()
-    cursor = db.cursor()
-
-    query = ''
-
-    if len(details)==0:
-        query = "SELECT * FROM passwordmanager.entries"
-    else:
-        query = "SELECT * FROM passwordmanager.entries WHERE "
-
-        for i in details:
-            query += f"{i} = '{details[i]}' AND "
-        query = query[:-5]
-
-    cursor.execute(query)
-    results = cursor.fetchall()
-
-    if len(results)==0:
-        print("No such entry available.")
-    else:
-        table = PrettyTable(cursor.column_names)
-        for row in results:
-            table.add_row([row[0], row[1], row[2], row[3], "{hidden}"])
-        print(table)
-
-        if len(results)==1:
-            option = input("Do you want to retrieve the password for this entry? y or n? ")
-
-            while(option != 'y' and option != 'n'):
-                print("Answer either y or n.")
-                option = input("Do you want to retrieve the password for this entry? y or n? ")
-
-            if(option == 'y'):
-                masterkey = cipher.generateKey(masterpassword, device_secret)
-                password = cipher.decrypt(masterkey, row[4])
-                pyperclip.copy(password)
-                print("Password copied to clipboard")
-    
-    cursor.close()
-    db.close()
-    
-    return
-
 def copy_password(row_id, tv):
 
     details = {'sitename':tv.item(row_id, 'values')[0], 'url':tv.item(row_id, 'values')[1], 'email':tv.item(row_id, 'values')[2], 'username':tv.item(row_id, 'values')[3]}
