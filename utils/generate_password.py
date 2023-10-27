25ba839484bdedbdd96ce56bf8e58ae499e79665
theshreyasm <theshreyasm@gmail.com>
2023-10-27 10:44:32 +0530

    comments: generate_password.py

--- utils/generate_password.py
+++ utils/generate_password.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+# function to generate secure password
 def generatePassword():
 
+    # returns a random string of length 16 consisting of digits and letters
     return ''.join(random.choices(string.ascii_letters + string.digits, k = 16))
