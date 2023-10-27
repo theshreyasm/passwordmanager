cd488e19095aa42288aec72d4382ec190ed2c3c9
theshreyasm <theshreyasm@gmail.com>
2023-10-27 12:35:50 +0530

    comments: config.py

--- config.py
+++ config.py
@@ -7,17 +7,19 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 
-
+# function to generate a random string
 def generateRandomString(length):
 
     return ''.join(random.choices(string.ascii_letters + string.digits, k = length))
 
-
+# function to configure the database and store the master password
 def config():
 
+    # connect to the database
     db = dbconfig()
     cursor = db.cursor()
 
+    # create a new database called passwordmanager
     cursor.execute("CREATE DATABASE passwordmanager")
 
     # create table secrets to store hash of the masterpassword and the device secret
@@ -28,6 +30,7 @@ def config():
     query = "create table passwordmanager.entries (sitename TEXT NOT NULL, url TEXT, email TEXT, username TEXT, password TEXT NOT NULL)"
     cursor.execute(query)
 
+    # configure password manager window
     window = Tk()
     window.title("Configure Password Manager")
     window.geometry("700x300")
@@ -55,21 +58,28 @@ def config():
 
     def save(): 
 
+        # get the password and confirm password from the respective entry boxes
         password = password_entry.get()
         confirm_password = confirm_password_entry.get()
 
+        # check if password is empty
         if password == "":
             status_label.config(text="Password cannot be empty.")
         
+        # check if passwords do not match
         elif password != confirm_password:
             status_label.config(text="Passwords do not match.")
 
         else:
             
+            # generate a random salt. use it to hash the master password
             salt = generateRandomString(length = 25)
             hashedpass = hashlib.sha256((salt + password).encode()).hexdigest()
+
+            # generate a device secret
             device_secret = generateRandomString(length = 15)
 
+            # insert salt, hashed masterpassword nad device secret into the secrets table
             query = "INSERT INTO passwordmanager.secrets (salt, masterpassword_hash, device_secret) VALUES (%s, %s, %s)"
             val = (salt, hashedpass, device_secret)
             cursor.execute(query, val)
@@ -78,8 +88,10 @@ def config():
             cursor.close()
             db.close()
 
+            # destroy the configure password manager window
             window.destroy()
 
+            # warning window
             warning_window = Tk()
             warning_window.title("Warning")
             warning_window.resizable(False, False)
@@ -89,6 +101,7 @@ def config():
 
             return warning_window
     
+    # save button in the configure password manager window
     save_button = tk.Button(password_frame, text="Save Master Password", command=save)
     save_button.grid(row=3, columnspan=2, pady=20)
     return window
